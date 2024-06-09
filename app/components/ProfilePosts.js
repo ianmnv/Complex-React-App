916d5ff358e2ce2a8f0349df4718dfe9993afb62
Ian M. Nava Garduno <ianmng1@gmail.com>
2024-06-09 11:53:27 -0600

    Loading actual data in ViewSinglePost component

--- app/components/ProfilePosts.js
+++ app/components/ProfilePosts.js
@@ -11,7 +11,7 @@ function ProfilePosts() {
     async function fetchPosts() {
       try {
         const response = await Axios.get(`/profile/${username}/posts`);
-        console.log(response.data);
+        // console.log(response.data);
         setPosts(response.data);
         setIsLoading(false);
       } catch (e) {
