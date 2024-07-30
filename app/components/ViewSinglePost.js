ca6193a40a626f2753a4b5b6701375b32250b87a
Ian M. Nava Garduno <ianmng1@gmail.com>
2024-07-29 19:23:40 -0600

    Ability to delete posts done and making sure only author can edit and delete their post

--- app/components/ViewSinglePost.js
+++ app/components/ViewSinglePost.js
@@ -1,14 +1,20 @@
-import React, { useEffect, useState } from "react";
-import { useParams, Link } from "react-router-dom";
+import React, { useEffect, useState, useContext } from "react";
+import { useParams, Link, useNavigate } from "react-router-dom";
 import Axios from "axios";
 import ReactMarkdown from "react-markdown";
 import { Tooltip } from "react-tooltip";
+
+import StateContext from "../StateContext";
+import DispatchContext from "../DispatchContext";
 
 import Page from "./Page";
 import LoadingDotsIcon from "./LoadingDotsIcon";
 import NotFound from "./NotFound";
 
 function ViewSinglePost() {
+  const navigate = useNavigate();
+  const appState = useContext(StateContext);
+  const appDispatch = useContext(DispatchContext);
   const [isLoading, setIsLoading] = useState(true);
   const [post, setPost] = useState();
   const { id } = useParams();
@@ -50,29 +56,60 @@ function ViewSinglePost() {
     year: "numeric",
   }).format(now);
 
+  function isOwner() {
+    if (appState.loggedIn) {
+      return appState.user.username === post.author.username;
+    }
+    return false;
+  }
+
+  async function deleteCB() {
+    const areYouSure = window.confirm("Are you sure to delete this post?");
+
+    if (areYouSure) {
+      try {
+        const response = await Axios.delete(`/post/${id}`, {
+          data: { token: appState.user.token },
+        });
+        console.log(response.data);
+        if (response.data === "Success") {
+          // 1. Display flash message
+          appDispatch({ type: "flashMsg", value: "Post successfully deleted" });
+          // 2. Redirect to home page
+          navigate(`/profile/${appState.user.username}`);
+        }
+      } catch (err) {
+        console.error(`${err}, There was a problem`);
+      }
+    }
+  }
+
   return (
     <Page title={post.title}>
       <div className="d-flex justify-content-between">
         <h2>{post.title}</h2>
-        <span className="pt-2">
-          <Link
-            to={`/post/${id}/edit`}
-            data-tooltip-content="Edit"
-            data-tooltip-id="edit"
-            className="text-primary mr-2"
-          >
-            <i className="fas fa-edit"></i>
-          </Link>
-          <Tooltip id="edit" className="custom-tooltip" />{" "}
-          <a
-            data-tooltip-content="Delete"
-            data-tooltip-id="delete"
-            className="delete-post-button text-danger"
-          >
-            <i className="fas fa-trash"></i>
-          </a>
-          <Tooltip id="delete" className="custom-tooltip" />
-        </span>
+        {isOwner() && (
+          <span className="pt-2">
+            <Link
+              to={`/post/${id}/edit`}
+              data-tooltip-content="Edit"
+              data-tooltip-id="edit"
+              className="text-primary mr-2"
+            >
+              <i className="fas fa-edit"></i>
+            </Link>
+            <Tooltip id="edit" className="custom-tooltip" />{" "}
+            <a
+              onClick={deleteCB}
+              data-tooltip-content="Delete"
+              data-tooltip-id="delete"
+              className="delete-post-button text-danger"
+            >
+              <i className="fas fa-trash"></i>
+            </a>
+            <Tooltip id="delete" className="custom-tooltip" />
+          </span>
+        )}
       </div>
 
       <p className="text-muted small mb-4">
